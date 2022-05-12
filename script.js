8562d7f3f56f237689b0026fe25bbd3b67cf48ec
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-05-12 23:09:12 +0200

    feat: complete burger menu

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const burgerWrapper = document.querySelector(".burger-wrapper");
 const burgerMenu = document.querySelector(".burger-menu");
 const menu = document.querySelector(".menu");
+const menuSpan = document.querySelector(".menu-span");
 
 burgerWrapper.addEventListener("click", function () {
-  burgerMenu.classList.add("rotate");
+  burgerMenu.classList.toggle("rotate");
   menu.classList.toggle("open");
+  if (menu.classList.contains("open")) {
+    menuSpan.innerHTML = "close";
+    menuSpan.style.color = "#ffffff";
+  } else {
+    menuSpan.innerHTML = "menu";
+    menuSpan.style.color = "#8e80a9";
+  }
 });
